73389eb82d390d16b23afb8ea2468ca8a9798353
AliDemir140 <alidemirr1400@gmail.com>
2022-11-21 17:02:53 +0300

    a

--- PINTERESTVIEWSPROJECT/pinterestviews/asgi.py
+++ PINTERESTVIEWSPROJECT/pinterestviews/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pinteresoo project.
+ASGI config for pinterestviews project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pinteresoo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pinterestviews.settings')
 
 application = get_asgi_application()
--- PINTERESTVIEWSPROJECT/pinterestviews/urls.py
+++ PINTERESTVIEWSPROJECT/pinterestviews/urls.py
@@ -1,4 +1,4 @@
-"""pinteresoo URL Configuration
+"""pinterestviews URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pinterest import views as pinterestViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', pinterestViews.home),
+     path('about/', pinterestViews.about),
 ]
