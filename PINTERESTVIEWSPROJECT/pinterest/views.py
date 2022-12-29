ec8b36ff14bcfc3819fa2466978539e2d3c6610f
AliDemir140 <alidemirr1400@gmail.com>
2022-12-29 11:22:50 +0300

    ch6

--- PINTERESTVIEWSPROJECT/pinterest/views.py
+++ PINTERESTVIEWSPROJECT/pinterest/views.py
@@ -2,21 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Pinterest
 
+
 def home(request):
- searchTerm = request.GET.get('searchPinterest')
- return render(request, 'home.html', 
- {'searchTerm':searchTerm})
+    searchTerm = request.GET.get('searchPinterest')
+    pinterests = Pinterest.objects.all()
+    return render(request, 'home.html',
+    {'searchTerm': searchTerm, 'pinterests': pinterests})
+
 
 def about(request):
- return render(request, 'about.html')
+    return render(request, 'about.html')
+
 
 def signup(request):
- email = request.GET.get('email')
- return render(request, 'signup.html', {'email':email})
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email': email})
+
 
 def news(request):
- searchTerm = request.GET.get('searchPinterest')
- return render(request, 'news.html', 
- {'searchTerm':searchTerm})
+    searchTerm = request.GET.get('searchPinterest')
+    return render(request, 'news.html',
+                  {'searchTerm': searchTerm})
 
 # Create your views here.
--- PINTERESTVIEWSPROJECT/pinterestviews/settings.py
+++ PINTERESTVIEWSPROJECT/pinterestviews/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pinterest.apps.PinterestConfig',
+    'pinterest',
+    'news',
+
 ]
 
 MIDDLEWARE = [
@@ -130,3 +133,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
